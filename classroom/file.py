a05eb95f6f73f68211d4d4460c9eb42da0ed87fe
jkfx <377910451@qq.com>
2019-05-21 21:25:52 +0800

    2019/5/21

--- classroom/file.py
+++ classroom/file.py
@@ -4,7 +4,6 @@ def isnumber(str1):
         if i >= '0' and i<='9':
             count += 1
     return count
-t1 = open('fx.txt')
-list1 = t1.readlines()
+list1 = ['geek666', '888fx']
 list2 = list(map(isnumber, list1))
 print(list2)
